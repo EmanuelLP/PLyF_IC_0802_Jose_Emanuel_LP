fbfe951bcab78dd7db83f78481bb47d7eb9d9b4b
EmanuelLP <pa3z2000@gmail.com>
2022-04-22 10:35:28 -0500

    Otras formas
    Se agregaron otras formas de solucion

--- 10_Calculadora_ambiguedad/Principal.java
+++ 10_Calculadora_ambiguedad/Principal.java
@@ -2,17 +2,50 @@
 
 public class Principal{
 	public static void main(String[]args){
-		Principal.engine((x,y) -> x + y);
-		Principal.engine((x,y) -> x * y);
-		Principal.engine((x,y) -> x / y);
-		Principal.engine((x,y) -> x - y);
-		Principal.engine((x,y) -> x % y);
+		//Primera forma
+	/*	Principal.engine((int x,int y) -> x + y);
+		Principal.engine((int x,int y) -> x * y);
+		Principal.engine((int x,int y) -> x / y);
+		Principal.engine((int x,int y) -> x - y);
+		Principal.engine((int x,int y) -> x % y); 
 
-		Principal.engines((x,y) -> (int) x + (int)y);
+		Principal.engine((long x,long y) -> (int) x + (int) y);
+		Principal.engine((long x,long y) -> (int) x * (int) y);
+		Principal.engine((long x,long y) -> (int) x / (int) y);
+		Principal.engine((long x,long y) -> (int) x - (int) y);
+		Principal.engine((long x,long y) -> (int) x % (int) y); */
+		
+		//Segunda forma
+		Principal.engine((CalculadoraInt)( x, y) -> x + y);
+		Principal.engine((CalculadoraInt)( x, y) -> x * y);
+		Principal.engine((CalculadoraInt)( x, y) -> x / y);
+		Principal.engine((CalculadoraInt)( x, y) -> x - y);
+		Principal.engine((CalculadoraInt)( x, y) -> x % y);
+
+		Principal.engine((CalculadoraLong)( x, y) ->  (int)(x + y));
+		Principal.engine((CalculadoraLong)( x, y) ->  (int)(x * y));
+		Principal.engine((CalculadoraLong)( x, y) ->  (int)(x / y));
+		Principal.engine((CalculadoraLong)( x, y) ->  (int)(x - y));
+		Principal.engine((CalculadoraLong)( x, y) ->  (int)(x % y)); 
+
+		//Tercera solucion
+
+		/*Principal.engine(objInt);
+		Principal.engine(objLong);*/
+
+
+
+/*		Principal.engine((long x,long y) -> x + y);
+		Principal.engine((long x,long y) -> x * y);
+		Principal.engine((long x,long y) -> x / y);
+		Principal.engine((long x,long y) -> x - y);
+		Principal.engine((long x,long y) -> x % y); */
+
+		/*Principal.engines((x,y) -> (int) x + (int)y);
 		Principal.engines((x,y) -> (int) x * (int) y);
 		Principal.engines((x,y) -> (int) x / (int) y);
 		Principal.engines((x,y) -> (int) x - (int) y);
-		Principal.engines((x,y) -> (int) x % (int) y);
+		Principal.engines((x,y) -> (int) x % (int) y);*/
 	}
 
 	//Sobrecarga de Métodos
@@ -22,10 +55,11 @@ public class Principal{
 		System.out.println("resultado = " + resultado);
 	}
 
-	private static void engines(CalculadoraLong cal){
+	private static void engine(CalculadoraLong cal){
 		int x = 2, y = 4;
 		int resultado = cal.calcular(x, y);
 		System.out.println("resultado = " + resultado);
 	}
+
 
 }
