86cc4eddad4b655afd160aa95109a59c3c8e9b01
EmanuelLP <pa3z2000@gmail.com>
2022-04-26 07:36:35 -0500

    ..

--- 13_Ejercicio/Principal.java
+++ 13_Ejercicio/Principal.java
@@ -4,7 +4,7 @@ public class Principal{
 	public static void main(String[]args){
 		
 			Principal.engine((int x,int y) -> x + y);
-			() -> return Principal.engine((int x,int y) -> x + y); 
+			//() -> return Principal.engine((int x,int y) -> x + y); 
 		
 	}
 
@@ -21,5 +21,10 @@ public class Principal{
 		System.out.println("resultado = " + resultado);
 	}
 
-
+	private static CalculadoraInt engine(){
+		int x = 2, y = 4;
+		//int resultado = cal.calcular(x, y);
+		//System.out.println("resultado = " + resultado);	
+		return 	calcular(x,y);
+	}
 }
