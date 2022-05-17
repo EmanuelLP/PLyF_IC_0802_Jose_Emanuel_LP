1e776a06fe0a633557d5cdf442e3ca652e714e68
EmanuelLP <pa3z2000@gmail.com>
2022-05-17 07:46:09 -0500

    Examen 3° Unidad
    Examen Practico

---
+++ Examen_practico_tercera_evaluacion/Principal.java
@@ -0,0 +1,25 @@
+public class Principal(){
+	
+	public static void main(String [] args){
+		prueba_examen.pe;
+		pe -> System.out.print(","+(int a1 + int b1 + int c1));
+		System.out.print(","+(int)operacion_prueba(int a2, int b2, int c2));
+		Principal.mensaje_hola();
+		String menj= "mensaje";
+		Princiapl.mensaje_hola(menj);
+		pe -> System.out.println(","+(int a3 + (int b3*int c3) ));
+		pe.operacion_prueba(3,operacion_prueba(5,10), operacion_prueba(5,10));
+		Principal.mi_metodo(pe);
+		Principal.mi_metodo( -> System.out.println(","+(int a4+(int b4/ int c4))));
+		pe -> System.out.println(","+((double)+(a5+(b5/c5))));
+		Principal.mi_metodo(pe,5,6,7);
+	}
+
+	private static void mi_metodo(prueba_examen prueba){
+		pe.operacion(10, prueba(2,4), prueba(6,8));
+	}
+
+	private static void mi_metodo(prueba examen prueba, int a, int b, int c){
+		mi_metodo.prueba(a, operacion_prueba(b,c),c);
+	}
+}
---
+++ Examen_practico_tercera_evaluacion/prueba_examen.java
@@ -0,0 +1,17 @@
+public interface prueba_examen{
+	
+	public void mensaje_hola(){
+		System.out.print("Hola mi nombre es José Emanuel López Paez");
+	}
+
+	public default void mensaje_hola(String mensaje){
+		System.out.print("La cadena es " + mensaje);
+	}
+
+	public static int operacion_prueba(int a, int b){
+		return (a+b);
+	}
+
+	public abstrac void operacion_prueba(int a, int b, int c){}
+	
+}
